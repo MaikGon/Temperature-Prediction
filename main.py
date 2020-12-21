8758c77b2f86f60d91374b84f03640387ce24d50
MaikGon <michal.gontarczyk@wp.pl>
2020-12-21 18:01:01 +0100

    Initial operations

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import sktime
-import fbprophet
+# import fbprophet
 import pmdarima
 import statsmodels.tsa
 import pyts
@@ -18,16 +18,49 @@ import random
 random.seed(42)
 
 
-def load_data():
+def example_tasks():
+    dates = pd.date_range('20191204', periods=30, freq='5s')
+    df = pd.DataFrame(
+        {'power': np.random.randint(low=0, high=50, size=len(dates))},
+        index=dates
+    )
+
+    df_resampled_mean = df.resample('6s').mean()
+    df_resampled_nearest = df.resample('6s').nearest()
+    df_resampled_ffill = df.resample('6s').ffill()
+    df_resampled_bfill = df.resample('6s').bfill()
+
+    df_plt, = plt.plot(df)
+    mean_plt, = plt.plot(df_resampled_mean)
+    nearest_plt, = plt.plot(df_resampled_nearest)
+    ffill_plt, = plt.plot(df_resampled_ffill)
+    bfill_plt, = plt.plot(df_resampled_bfill)
+    plt.legend(
+        [df_plt, mean_plt, nearest_plt, ffill_plt, bfill_plt],
+        ['df', 'mean', 'nearest', 'ffill', 'bfill'],
+        loc='upper right'
+    )
+    plt.show()
+
+
+def read_temp_mid_sn():
     with open('dataset/additional_info.json') as f:
         additional_data = json.load(f)
 
-    print(additional_data)
-
     devices = additional_data['offices']['office_1']['devices']
-    sn_temp_nid = [d['serialNumber'] for d in devices if d['description'] == 'temperature_middle']
-    print(sn_temp_nid)
+    sn_temp_mid = [d['serialNumber'] for d in devices if d['description'] == 'temperature_middle']
+    print("Mid serial number: ", sn_temp_mid)
+    return sn_temp_mid
+
+
+def project_check_data():
+    sn_temp_mid = read_temp_mid_sn()
+    df_temp = pd.read_csv('dataset/office_1_temperature_supply_points_data_2020-10-13_2020-11-02.csv')
+    print(df_temp.info())
+    print(df_temp.describe())
+    print(df_temp.head(5))
 
 
 if __name__ == "__main__":
-    load_data()
+    # example_tasks()
+    project_check_data()
