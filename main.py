396e23849d669ef9431a2c98c83b094aac23cdc2
MaikGon <michal.gontarczyk@wp.pl>
2020-12-21 22:27:29 +0100

    Some data analysis

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import random
 random.seed(42)
 
 
+# Training
 def example_tasks():
     dates = pd.date_range('20191204', periods=30, freq='5s')
     df = pd.DataFrame(
@@ -43,24 +44,67 @@ def example_tasks():
     plt.show()
 
 
+# Project part
 def read_temp_mid_sn():
     with open('dataset/additional_info.json') as f:
         additional_data = json.load(f)
 
     devices = additional_data['offices']['office_1']['devices']
+
     sn_temp_mid = [d['serialNumber'] for d in devices if d['description'] == 'temperature_middle']
     print("Mid serial number: ", sn_temp_mid)
-    return sn_temp_mid
+    return sn_temp_mid[0]
+
+
+def read_data(name):
+    df = pd.read_csv('dataset/' + name)
+    df.rename(columns={'Unnamed: 0': 'time'}, inplace=True)
+    df['time'] = pd.to_datetime(df['time'])
+    df.drop(columns=['unit'], inplace=True)
+    df.set_index('time', inplace=True)
+
+    return df
 
 
 def project_check_data():
     sn_temp_mid = read_temp_mid_sn()
-    df_temp = pd.read_csv('dataset/office_1_temperature_supply_points_data_2020-10-13_2020-11-02.csv')
-    print(df_temp.info())
-    print(df_temp.describe())
-    print(df_temp.head(5))
+    print("Mid serial number: ", sn_temp_mid)
+
+    # Temperature
+    df_temp = read_data('office_1_temperature_supply_points_data_2020-10-13_2020-11-02.csv')
+
+    # Serial number for middle
+    df_temp = df_temp[df_temp['serialNumber'] == sn_temp_mid]
+
+    # Target
+    df_target = read_data('office_1_targetTemperature_supply_points_data_2020-10-13_2020-11-01.csv')
+    df_target.rename(columns={'value': 'target_temp'}, inplace=True)
+    # Valve
+    df_valve = read_data('office_1_valveLevel_supply_points_data_2020-10-13_2020-11-01.csv')
+
+    # Show some info
+    # print(df_temp.info())
+    # print(df_temp.describe())
+    # print(df_temp.head(5))
+
+    df_combined = pd.concat([df_temp, df_target], sort='time')
+    df_combined = df_combined.resample(pd.Timedelta(minutes=15)).mean().fillna(method='ffill')
+
+    print(df_combined.head(5))
+    df_combined.plot()
+    plt.figure()
+
+    plt.plot(df_temp.index, df_temp.value)
+    plt.plot(df_target.index, df_target.target_temp)
+
+    plt2 = plt.twinx()
+    # plt2.plot(df_valve.index, df_valve.value, color='g')
+    plt.show()
 
 
 if __name__ == "__main__":
-    # example_tasks()
+    temp = 'office_1_temperature_supply_points_data_2020-10-13_2020-11-02.csv'
+    target = 'office_1_targetTemperature_supply_points_data_2020-10-13_2020-11-01.csv'
+    valve = 'office_1_valveLevel_supply_points_data_2020-10-13_2020-11-01.csv'
+
     project_check_data()
